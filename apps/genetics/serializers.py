2d834930be289a2b7e99c0519d8133e25809f64f
arolper <arolper@users.noreply.github.com>
2024-06-13 09:37:32 +0000

    Auto-commit ruff style fixes

--- apps/genetics/serializers.py
+++ apps/genetics/serializers.py
@@ -1,25 +1,26 @@
 from common.utils.serializers import CaseModelSerializer
 from .models import Gene, Product, Produces, GeneticFeatures
 
+
 class GeneSerializer(CaseModelSerializer):
-    class Meta:
-        model = Gene
-        fields = '__all__'
+	class Meta:
+		model = Gene
+		fields = "__all__"
 
 
 class ProductSerializer(CaseModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
+	class Meta:
+		model = Product
+		fields = "__all__"
 
 
 class ProducesSerializer(CaseModelSerializer):
-    class Meta:
-        model = Produces
-        fields = '__all__'
+	class Meta:
+		model = Produces
+		fields = "__all__"
 
 
 class GeneticFeaturesSerializer(CaseModelSerializer):
-    class Meta:
-        model = GeneticFeatures
-        fields = '__all__'
+	class Meta:
+		model = GeneticFeatures
+		fields = "__all__"

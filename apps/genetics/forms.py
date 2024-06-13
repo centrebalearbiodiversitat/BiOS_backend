cfab088df172c74a2771e421c9713c10b5106d08
arolper <alejandro.roldan@uib.cat>
2024-06-13 11:34:39 +0200

    image_id field for uploading images has been changed to image_path

---
+++ apps/genetics/forms.py
@@ -0,0 +1,75 @@
+from common.utils.forms import CamelCaseForm, IdFieldForm
+from .models import Gene, Product, Produces, GeneticFeatures
+from django import forms
+
+
+class GeneForm(IdFieldForm, CamelCaseForm):
+	sources = forms.IntegerField(
+		required=False)
+	exact = forms.BooleanField(required=False)
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields["batch"].required = False
+		self.fields["name"].required = False
+		self.fields["unidecode_name"].required = False
+		self.fields["sources"].required = False
+
+	class Meta:
+		model = Gene
+		fields = "__all__"
+
+class ProductForm(IdFieldForm, CamelCaseForm):
+	sources = forms.IntegerField(required=False)
+	exact = forms.BooleanField(required=False)
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields["batch"].required = False
+		self.fields["name"].required = False
+		self.fields["unidecode_name"].required = False
+		self.fields["sources"].required = False
+
+	class Meta:
+		model = Product
+		fields = "__all__"
+
+class ProducesForm(IdFieldForm, CamelCaseForm):
+	sources = forms.IntegerField(required=False)
+
+	class Meta:
+		model = Produces
+		fields = "__all__"
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields["batch"].required = False
+		self.fields["sources"].required = False
+		self.fields["gene"].required = False
+		self.fields["product"].required = False
+
+
+class GeneticFeaturesForm(IdFieldForm, CamelCaseForm):
+	sources = forms.IntegerField(required=False)
+	exact = forms.BooleanField(required=False)
+
+	class Meta:
+		model = GeneticFeatures
+		fields = "__all__"
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields["batch"].required = False
+		self.fields["sources"].required = False
+		self.fields["occurrence"].required = False
+		self.fields["isolate"].required = False
+		self.fields["bp"].required = False
+		self.fields["definition"].required = False
+		self.fields["data_file_division"].required = False
+		self.fields["published_date"].required = False
+		self.fields["collection_date_year"].required = False
+		self.fields["collection_date_month"].required = False
+		self.fields["collection_date_day"].required = False
+		self.fields["molecule_type"].required = False
+		self.fields["sequence_version"].required = False
+		self.fields["products"].required = False
---
+++ apps/genetics/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import (
+    GeneCRUDView,
+    GeneDetailView,
+    GeneListView,
+    ProductCRUDView,
+    ProductDetailView,
+    ProductListView,
+    ProducesCRUDView,
+    ProducesListView,
+    GeneticFeaturesCRUDView,
+    GeneticFeaturesDetailView,
+    GeneticFeaturesListView,
+)
+urlpatterns = [
+    path('gene', GeneCRUDView.as_view()),
+    path('gene/search', GeneDetailView.as_view()),
+    path('gene/list', GeneListView.as_view()),
+
+    path('product', ProductCRUDView.as_view()),
+    path('product/search', ProductDetailView.as_view()),
+    path('product/list', ProductListView.as_view()),
+
+    path('produces', ProducesCRUDView.as_view()),
+    path('produces/list', ProducesListView.as_view()),
+
+    path('genetic-features', GeneticFeaturesCRUDView.as_view()),
+    path('genetic-features/search', GeneticFeaturesDetailView.as_view()),
+    path('genetic-features/list', GeneticFeaturesListView.as_view()),
+]

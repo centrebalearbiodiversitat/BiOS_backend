1176a57999bee7b5cb05a5ac368228d7edf327fb
ToGo347 <ToGo347@users.noreply.github.com>
2024-06-20 11:56:26 +0000

    Auto-commit ruff style fixes

--- apps/genetics/urls.py
+++ apps/genetics/urls.py
@@ -11,7 +11,8 @@ from .views import (
 	SequenceCRUDView,
 	SequenceListView,
 	TaxonGeneticView,
-	TaxonGeneticCountView, SequenceSearchView,
+	TaxonGeneticCountView,
+	SequenceSearchView,
 )
 
 urlpatterns = [

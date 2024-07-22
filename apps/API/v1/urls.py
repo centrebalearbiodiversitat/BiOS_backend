0e8cc441eb81a30d7c6d003ff67c2ffad7bb890c
arolper <160747398+arolper@users.noreply.github.com>
2024-07-22 12:32:06 +0200

    Update urls.py

--- apps/API/v1/urls.py
+++ apps/API/v1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
 	path("/taxonomy", include("apps.taxonomy.urls")),
-	path("/occurrence", include("apps.occurrences.urls")),
+	path("/occurrences", include("apps.occurrences.urls")),
 	path("/geography", include("apps.geography.urls")),
 	path("/genetics", include("apps.genetics.urls")),
 	path("/versioning", include("apps.versioning.urls")),

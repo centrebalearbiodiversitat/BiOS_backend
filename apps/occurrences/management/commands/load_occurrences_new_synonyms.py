d146bb6edb7bff60beb3b8ec2b4094e5522d137f
tomascbb <tomas.golomb@uib.cat>
2025-02-07 16:40:48 +0100

    fixed date parser

--- apps/occurrences/management/commands/load_occurrences_new_synonyms.py
+++ apps/occurrences/management/commands/load_occurrences_new_synonyms.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.utils.dateparse import parse_datetime
+from dateutil import parser
 from django.contrib.gis.geos import Point, GEOSGeometry
 from apps.genetics.models import Sequence, Marker, Product
 from apps.occurrences.models import Occurrence
@@ -114,7 +114,7 @@ def genetic_sources(line: dict, batch, occ):
 			batch=batch,
 			isolate=line["isolate"],
 			definition=line["definition"],
-			published_date=parse_datetime(line["date"]) if line["date"] else None,
+			published_date=parser.parse(line["date"]) if line["date"] else None,
 		)
 		seq.sources.add(os)
 

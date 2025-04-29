14a606ef56ef80c6ac7c4345dc75a008e5116fe7
TommasoCanc <tommaso.cancellario@gmail.com>
2025-04-29 17:01:43 +0200

    docs: suggest improvements for genetic API endpoint descriptions

--- apps/API/views.py
+++ apps/API/views.py
@@ -6,7 +6,10 @@ from apps.API.serializers import APIStatusSerializer
 
 
 class APIStatus(APIView):
-	@swagger_auto_schema(operation_description="Check current API status", responses={200: APIStatusSerializer()})
+	@swagger_auto_schema(
+		operation_description="Check current API status.",
+		responses={200: APIStatusSerializer()}
+	)
 	def get(self, request):
 		return Response(
 			{

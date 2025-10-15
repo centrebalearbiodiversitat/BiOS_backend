826751816d46e504a3f5e250abf55cf84c11e3a1
ToGo347 <20760203+ToGo347@users.noreply.github.com>
2025-10-15 13:00:26 +0000

    Auto-commit ruff style fixes

--- apps/versioning/views.py
+++ apps/versioning/views.py
@@ -347,9 +347,11 @@ class SourceStatisticsFilter(APIView):
 			),
 			all=True,
 		)
-		print(Source.objects.filter(basis_id=basis_id, data_type=Source.TAXON_DATA).annotate(
-				count=Coalesce(Subquery(oq_taxon_data[:1]), 0)
-			).values())
+		print(
+			Source.objects.filter(basis_id=basis_id, data_type=Source.TAXON_DATA)
+			.annotate(count=Coalesce(Subquery(oq_taxon_data[:1]), 0))
+			.values()
+		)
 		return sources.union(sources)
 
 
